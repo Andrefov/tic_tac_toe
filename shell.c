0b198056d57f1ad0d03b50af342a71b095ee7585
KrystianKiznar <krystian.kiznar@gmail.com>
2022-05-30 23:20:04 +0200

    new functions created (select_mode, Ai_level)

--- shell.c
+++ shell.c
@@ -1,7 +1,65 @@
 #include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+
+int select_mode()
+{
+	int option;
+	printf("Select mode:\n1.Player vs Player\n2.Player vs Computer\n3.Computer vs Computer \n>>>");
+	scanf_s("%d", &option);
+
+	switch (option)
+	{
+		case 1:
+			printf("Player vs Player mode\n!");
+			return 1;
+		case 2:
+			printf("Player vs Computer mode!\n");
+			return 2;
+		case 3:
+			printf("Computer vs Computer mode\n");
+			return 3;
+		default:
+			printf("Unknown option\n");
+			return -1;
+	}
+}
+
+int Ai_level()
+{
+	int option;
+	printf("Select AI level:\n1.Easy\n2.Hard");
+	scanf_s("%d", &option);
+
+	switch (option)
+	{
+	case 1:
+		printf("Easy!\n");
+		return 1;
+	case 2:
+		printf("Hard!\n");
+		return 2;
+	default:
+		printf("Unknown option!\n");
+		return -1;
+	}
+}
+
+int select_symbol()
+{
+
+}
+
 
 int main()
 {
-	printf("sprawdzenie");
+	int x;
+
+	x = select_mode();
+
+	printf("%d. mode has been choosen!", x);
+
 	return 0;
+
 }

d0075a1a70f1e50b1bb04ba77164246180938b9b
KrystianKiznar <krystian.kiznar@gmail.com>
2022-06-01 15:19:57 +0200

    name comparing has been added

--- shell.c
+++ shell.c
@@ -3,11 +3,9 @@
 #include<ctype.h>
 
 int selected_mode=0, player1_selected_symbol = 0, player2_selected_symbol = 0, one_more = 0, AI_level = 0;     //selected mode: int 1 or 2 or 3
-char* player1_name[8], player2_name[8];
+char player1_name[10], player2_name[10];
 
 int readline(char* line, size_t len);
-
-const char* get_name();
 
 int select_mode();
 
@@ -25,10 +23,6 @@ int take_coordinate();
 
 int display_menu();
 
-void choose_my_name(char* imie)
-{
-	fgets(imie, 30, stdin);
-}
 
 int main()
 {
@@ -40,10 +34,6 @@ int main()
 	printf("player1 name is %s", player1_name);
 
 
-	//char* player1name = get_name();
-	//printf("%s", player1name);
-	//settings:
-	
 	return 0;
 }
 
@@ -104,17 +94,11 @@ int readline(char* line, size_t len)
 	line[strlen(line) - 1] = '\0';
 }
 
-const char* get_name()
-{
-	// user sets a name
-	char* name[8];
-	readline(name, sizeof(name));
-
-	return name;
-}
 
 int select_mode()
-{//user chooses the game mode
+{//user chooses the game mode. Function returns int: 1 - Player vs Player mode, 
+	                                             //  2 - Player vs Computer mode
+												 //  3 - Computer vs Computer mode
 	int option = 0;
 	char* line[1];
 
@@ -130,12 +114,24 @@ int select_mode()
 			printf("Player vs Player mode!\n");
 
 			printf("Enter the first player's name\n>>>");
-			choose_my_name(player1_name);
+			readline(player1_name, sizeof(player1_name));
 
 			player1_selected_symbol = select_symbol();
-
-			printf("Enter the second player's name\n>>>");
-			choose_my_name(player2_name);
+			while (1)
+			{
+				printf("Enter the second player's name\n>>>");
+				readline(player2_name, sizeof(player2_name));
+				
+				if (strcmp(player1_name, player2_name) == 0)
+				{
+					printf("Users names can not be the same! Try again!\n");
+					continue;
+				}
+				else
+				{
+					break;
+				}
+			}
 			player2_selected_symbol = select_symbol();
 
 			return 1;
@@ -144,7 +140,9 @@ int select_mode()
 		{
 			printf("Player vs Computer mode!\n");
 			printf("Enter the first player's name\n>>>");
-			choose_my_name(player1_name);
+
+			readline(player1_name, sizeof(player1_name));
+
 			player1_selected_symbol = select_symbol();
 			AI_level = Ai_level();
 			return 2;
@@ -165,7 +163,8 @@ int select_mode()
 
 int Ai_level()
 {
-	//user can choose the AI level
+	//user can choose the AI level. Returns int 1. Easy
+											 // 2. Hard
 	int level = 0;
 	char* line[1];
 	printf("Select AI level:\n1.Easy\n2.Hard \n>>>");
@@ -196,7 +195,8 @@ int Ai_level()
 
 int select_symbol()
 {
-	//user can choose the symbol
+	//user can choose the symbol. Returns int 1. X
+											//2. O
 	int symbol = 0;
 	char* line[1];
 
@@ -227,7 +227,7 @@ int select_symbol()
 }
 
 void end_game_results(int result)
-{	//func gives game results
+{	//func prints game results.
 	if (result == 1)
 	{
 		printf("You win!\n");
@@ -244,7 +244,10 @@ void end_game_results(int result)
 
 int one_more_game()
 {
-	//func asks user if he want to play again
+	//func asks user if he want to play again. Takes his imput from board and returns
+	                                                            // 1. if he want to play again
+																//-1. if he doen't want to play again.Quit game
+																// 0. if he want to return to main manu
 	char choice[6];
 
 	printf("Do you want play again? Type 'yes', 'no' or 'quit' to return to main menu. \n>>>");
@@ -290,7 +293,7 @@ void help_menu()
 
 int take_coordinate()
 {
-	// func takes coordinate from user
+	// Func takes coordinate from user and returns it.
 	int cor;
 	char* line[1];
 	printf("Enter coordinate (1-9 fields)\n>>>");

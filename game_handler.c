da0a9affcbadb6f9e7bfb8b0a5ae831ef0a48a94
XVND83 <XVND83@XVND83-Z14G8>
2022-06-02 15:01:44 +0200

    player vs player - done

--- game_handler.c
+++ game_handler.c
@@ -3,10 +3,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include "shell.c"
-
-// commented only for local tests
-//#include "statistic.h" // this wil be required to be able te define the structures in the same way
-// structures in statistic.c could also be defined with as 'extern'
+#include "board.c""
+#include "statistic.h"
 
 #pragma warning(disable : 4996)
 int gh_game_mode;
@@ -50,6 +48,96 @@ void increase_time() {
 	gh_ps2.time + gh_game_time;
 }
 
+void print_game_state()
+{
+	//Game Mode from Shell:  1.Player vs Player 2.Player vs Computer 3.Computer vs Computer
+	// int gameStatus (infro from baord)
+	//	-1 - initial and in progress
+	//	0 - tie
+	//	1 - somebody won
+	increase_time();
+
+	switch (gh_game_status)
+	{
+	case -1:
+		printf("Game in progress..\n");
+		break;
+
+
+	case 0: // draw/tie
+		printf("\nGame over!. Nobody won.\n");
+		gh_gs.draw++;
+		gh_gs.rounds++;
+
+		// dopisanie remisu i rundy do pierwszego gracza 
+		gh_ps1.draw++;
+		gh_ps1.rounds++;
+		gh_ps1.moves = move_player1; // doliczenie ruchów
+
+		// dopisanie remisu i rundy do drugiego gracza 
+		gh_ps2.draw++;
+		gh_ps2.rounds++;
+		gh_ps2.moves = move_player2; // doliczenie ruchów
+		break;
+
+
+	case 1: // SOMEONE have won (there is a winner and looser)
+		printf("\nGame over!. We have a winner!\n");
+		//char playerMark - player that have won
+		//	' ' - initial
+		//	'O'
+		//	'X'
+		gh_gs.wins++;
+		gh_gs.losts++;
+		gh_gs.rounds++;
+
+		if (gh_game_mode > 1)
+		{
+			gh_gs.comp_vs++;
+			printf("global stats of comp vs were incremented\n");
+		}
+
+		if (gh_player_mark == 1) // zmienna z board który gracz wygrał
+		{
+			printf("Congratulations %s, you have won!\n", gh_ps1.name);
+			gh_ps1.wins++;
+			gh_ps1.rounds++;
+			gh_ps1.moves = move_player1; // doliczanie ruchów do wygranego
+
+			gh_ps2.losts++;
+			gh_ps2.rounds++;
+			gh_ps2.moves = move_player2;
+		}
+		else if (gh_player_mark == 2) {
+			printf("Congratulations %s, you have won!\n", gh_ps2.name);
+			gh_ps2.wins++;
+			gh_ps2.rounds++;
+			gh_ps2.moves = move_player2; // doliczanie ruchów dla wygranego
+
+			gh_ps1.losts++;
+			gh_ps1.rounds++;
+			gh_ps1.moves = move_player1;
+		}
+		else {
+			printf("\nGamer error, we have a winner, but don't know who that is!\n");
+		}
+		break;
+
+	default:
+		printf("\nGame error. Status unknown.\n");
+
+	}
+}
+
+
+void print_player_statistic(struct player_stats ps) {
+	printf("Player statistic for %s\n", ps.name);
+	printf("Time: %d", ps.time);
+
+}
+
+
+
 /*
 funkcja round
 sprawdzić game mode i dostosowac -
@@ -75,116 +163,76 @@ wywyływanie ruchu komputera 'calculate_move' - player number w środku
 wysyłać int - poziom trudności z shell
 poziom trudności - dummy_move
 
-select_mode
-1- player vs player
-2- player vs comp
-3- comp vs comp
-
 */
 
-void lauch_game() {
-	if (selected_mode == 1)
+void round() {
 
+	do	{
 
+		start_board();
+		selected_mode();
 
-}
+		if (selected_mode == 1) // player vs player
 
+			while (is_win() == -1) {
 
-void print_game_state() 
-{
-	//Game Mode from Shell:  1.Player vs Player 2.Player vs Computer 3.Computer vs Computer
-	// int gameStatus (infro from baord)
-	//	-1 - initial and in progress
-	//	0 - tie
-	//	1 - somebody won
-	increase_time();
+				if
+					which_player = 1
+					player_one_move(take_coordinate(which_player));
+				gh_ps1.move = move_player1;
 
-	switch (gh_game_status)
-	{
-	case -1:
-		printf("Game in progress..\n");
-		break;
+				else
+					which_player = 2
+					player_two_move((take_coordinate(which_player))
+						gh_ps2.move = move_player2;
+			}
+		print_game_state();
 
+	} while (one_more_game() == 1);
+	
 
-	case 0: // draw/tie
-		printf("\nGame over!. Nobody won.\n");
-		gh_gs.draw++; 
-		gh_gs.rounds++;
+	
+
+	if (selected_mode == 2) // player vs comp
+
+				switch (ai_level)
+				{
+				case 1: // poziom trudności dummy
+
+					dummy_move
+
+				case 2: // poziom trudności pro
+
+					pro_move
+
+				default:
+					break;
+				}
+	if (selected_mode == 3) // comp vs comp
+
+				switch (ai_level)
+				{
+				case 1: // poziom trudności dummy
+
+					dummy_move
+					player_move
+						_sleep
+
+				case 2: // poziom trudności pro
+
+					pro_move
+					player_move
+						_sleep
+
+				default:
+					break;
+				}
 		
 
-		if (gh_game_mode > 1 ) // ustalić na stałe imię compa
-		{
-			gh_gs.comp_vs++;
-			printf("global stats of comp vs were incremented\n");
-		}
-		// dopisanie remisu i rundy do pierwszego gracza 
-		gh_ps1.draw++;
-		gh_ps1.rounds++;
-		gh_ps1.moves + gh_game_moves; // doliczenie ruchów
-		printf("player number %s draw and rounds were incremented.\n", gh_ps1.name);
-		
-		// dopisanie remisu i rundy do drugiego gracza 
-		gh_ps2.draw++;
-		gh_ps2.rounds++;
-		gh_ps2.moves + gh_game_moves; // doliczenie ruchów
-		printf("player number %s draw and rounds were incremented.\n", gh_ps2.name);
-			
-		break;
+		how_long();
 
-
-	case 1: // SOMEONE have won (there is a winner and looser)
-		printf("\nGame over!. We have a winner!\n");
-		//char playerMark - player that have won
-		//	' ' - initial
-		//	'O'
-		//	'X'
-		gh_gs.wins++;
-		gh_gs.losts++;
-		gh_gs.rounds++;
-
-		if (gh_game_mode > 1)
-		{
-			gh_gs.comp_vs++;
-			printf("global stats of comp vs were incremented\n");
-		}
-
-		if (gh_player_mark == 1) // zmienna z board który gracz wygrał
-		{
-			printf("Congratulations %s, you have won!\n", gh_ps1.name);
-			gh_ps1.wins++;
-			gh_ps1.rounds++;
-			gh_ps1.moves + gh_game_moves; // doliczanie ruchów do wygranego
-			
-			gh_ps2.losts++;
-			gh_ps2.rounds++;
-			gh_ps2.moves + gh_game_moves - 1;
-		} else if (gh_player_mark == 2) {
-			printf("Congratulations %s, you have won!\n", gh_ps2.name);
-			gh_ps2.wins++;
-			gh_ps2.rounds++;
-			gh_ps2.moves + gh_game_moves; // doliczanie ruchów dla wygranego
-
-			gh_ps1.losts++;
-			gh_ps1.rounds++;
-			gh_ps1.moves + gh_game_moves - 1; 
-		} else {
-			printf("\nGamer error, we have a winner, but don't know who that is!\n");
-		}
-		break;
-
-	default:
-		printf("\nGame error. Status unknown.\n");
-
-	}
+	
 }
-
-
-void print_player_statistic(struct player_stats ps) {
-	printf("Player statistic for %s\n", ps.name);
-	printf("Time: %d", ps.time);
-
-}
-
 
 
 
